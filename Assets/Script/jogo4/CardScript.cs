61d38cd759c3bfe69945589c772becdd09be234a
Carlitxa <cmoutinho191@gmail.com>
2025-07-08 19:19:13 +0100

    Jogo4
    mais paginas e algumas mudanças nos scripts

--- Assets/Script/jogo4/CardScript.cs
+++ Assets/Script/jogo4/CardScript.cs
@@ -1,38 +1,69 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardScript : MonoBehaviour
 {
- public Image Below;
- public Image Cover;
- 
- public void Awake()
- {
-    Below.gameObject.SetActive(true);
-    Cover.gameObject.SetActive(true);
- }
+    public Image Below;
+    public Image Cover;
 
- public void SetBelowColor(Color newColor)
- {
-    Below.color = newColor;
- }
- 
- public void SetBelowImage(Sprite newImage)
- {
-    Below.color = Color.white;
-    Below.sprite = newImage;
- }
- 
- public void DisableCover()
- {
-    Cover.gameObject.SetActive(false);
- }
- 
- public void EnableCover()
-{
-    Cover.gameObject.SetActive(true);
- }
+    private bool isFlipping = false;
+    private bool isFaceUp = false;
+
+    public void Awake()
+    {
+        Below.gameObject.SetActive(true);
+        Cover.gameObject.SetActive(true);
+    }
+
+    public void SetBelowImage(Sprite newImage)
+    {
+        Below.color = Color.white;
+        Below.sprite = newImage;
+    }
+
+    public IEnumerator FlipCardCoroutine(float duration = 0.5f)
+    {
+        if (isFlipping) yield break;
+        isFlipping = true;
+
+        float halfDuration = duration / 2f;
+        float time = 0f;
+
+        Vector3 startScale = transform.localScale;
+        Vector3 midScale = new Vector3(0f, 1f, 1f);
+        Vector3 endScale = new Vector3(1f, 1f, 1f);
+
+        while (time < halfDuration)
+        {
+            float t = time / halfDuration;
+            transform.localScale = Vector3.Lerp(startScale, midScale, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = midScale;
+
+        // Toggle cover visibility
+        Cover.gameObject.SetActive(isFaceUp); // Show cover if it was face up
+
+        isFaceUp = !isFaceUp;
+
+        time = 0f;
+        while (time < halfDuration)
+        {
+            float t = time / halfDuration;
+            transform.localScale = Vector3.Lerp(midScale, endScale, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = endScale;
+
+        isFlipping = false;
+    }
+
+    public bool IsFaceUp {
+      get {return isFaceUp;}
+    }
 }
